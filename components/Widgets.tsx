086a17d856d405dda1248cabfe528db475fc9645
cobra11793 <vmakdesian@gmail.com>
2022-08-11 10:04:36 -0700

    Feed, Sidebar, Widget, SanityIO

---
+++ components/Widgets.tsx
@@ -0,0 +1,23 @@
+import { SearchCircleIcon, SearchIcon } from '@heroicons/react/outline'
+import React from 'react'
+import { TwitterTimelineEmbed } from 'react-twitter-embed'
+
+function Widgets() {
+    return (
+        <div className='px-2 mt-2 col-span-2 hidden lg:inline'>
+            <div className='mt-2 flex items-center space-x-2 bg-gray-100 p-3 rounded-full'>
+                <SearchIcon className="h-5 w-5 text-gray-600" />
+                <input className="flex-1 outline-none bg-transparent" type="text" placeholder="Search Twitter.." />
+            </div>
+
+            <TwitterTimelineEmbed
+                sourceType="profile"
+                screenName="linkedin"
+                options={{ height: 1000 }}
+            />
+
+        </div>
+    )
+}
+
+export default Widgets
