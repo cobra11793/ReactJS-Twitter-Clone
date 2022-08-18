3c42142f4416fabe7e510df86b03dfd76c846df3
cobra11793 <vmakdesian@gmail.com>
2022-08-17 20:33:55 -0700

    Feed & Tweet components, connect scheme backend (big changes)

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import { RefreshIcon } from '@heroicons/react/outline'
 import TweetBox from './TweetBox'
+import { Tweet } from '../typings'
+import TweetComponent from '../components/Tweet'
 
-function Feed() {
+interface Props {
+  tweets: Tweet[]
+}
+
+function Feed({tweets}: Props) {
   return (
   <div className='col-span-7 lg:col-span-5 border-x'>
     <div className='flex items-center justify-between'>
@@ -13,6 +19,13 @@ function Feed() {
     <div>
       <TweetBox/>
     </div>
+
+    <div>
+      {tweets.map(tweet =>(
+        <TweetComponent key={tweet._id} tweet={tweet}/>
+      ))}  
+    </div>
+
   </div>
   )
 }
