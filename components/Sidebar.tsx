2121ea7887c5daea59f45217d2134c9c7046f03e
cobra11793 <vmakdesian@gmail.com>
2022-09-30 10:05:53 -0700

    Implementing NextJs Auth

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import { BellIcon, HomeIcon, HashtagIcon, BookmarkIcon, CollectionIcon, DotsCircleHorizontalIcon, MailIcon, UserIcon, } from '@heroicons/react/outline'
 import SidebarRow from './SidebarRow'
+import { signIn, signOut, useSession } from 'next-auth/react'
 
 function Sidebar() {
+  const { data: session } = useSession()
+
   return (
     <div className='flex flex-col col-span-2 items-center px-4 md:items-start'>
       <img className="m-3 h-10 w-10" src="http://links.papareact.com/drq" alt="" />
@@ -12,8 +15,7 @@ function Sidebar() {
       <SidebarRow Icon = {MailIcon} title ="Messages"/>
       <SidebarRow Icon = {BookmarkIcon} title ="Bookmarks"/>
       <SidebarRow Icon = {CollectionIcon} title ="Lists"/>
-      <SidebarRow Icon = {UserIcon} title ="Sign In"/>
-
+      <SidebarRow onClick={session ? signOut : signIn}Icon = {UserIcon} title ={session ? 'Sign out': 'Sign in'}/>
       <SidebarRow Icon = {DotsCircleHorizontalIcon} title ="More"/>
     </div>
   )
--- components/SidebarRow.tsx
+++ components/SidebarRow.tsx
@@ -3,12 +3,14 @@ import React, {SVGProps} from 'react'
 interface Props {
     Icon: ((props: SVGProps<SVGSVGElement>) => JSX.Element)
         title: string
+        onClick?: () => {}
     
 }
 
-function SidebarRow({Icon, title}: Props) {
+function SidebarRow({Icon, title, onClick}: Props) {
   return (
-    <div className='flex items-center space-x-2 px-4 py-3 rounded-full hover:bg-gray-100 transition-all duration-200 cursor-pointer group max-w-fit'>
+    <div onClick={() => onClick?.()} 
+    className='flex items-center space-x-2 px-4 py-3 rounded-full hover:bg-gray-100 transition-all duration-200 cursor-pointer group max-w-fit'>
         <Icon className="h-6 w-6"/>
         <p className ="group-hover:text-twitter hidden md:inline-flex text-base font-light lg:text-xl">{title}</p>
     </div>
